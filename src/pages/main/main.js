e5d34dd024549a390de59fbc13073320a17ee610
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-04-06 23:24:45 +0500

    create header component

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,6 +1,7 @@
 /* Главная страница */
 import { AbstractView } from '../../common/view.js';
 import onChange from 'on-change';
+import { Header } from '../../components/header/header.js';
 export class MainView extends AbstractView {
   state = {
     list: [], //
@@ -12,21 +13,25 @@ export class MainView extends AbstractView {
   constructor(appState) {
     super();
     this.appState = appState;
-    this.appState = onChange(this.appState, this.appStateHook.bind(this))
+    this.appState = onChange(this.appState, this.appStateHook.bind(this));
     this.setTitle('Поиск книг');
   }
   /* Метод после обновления appState*/
   appStateHook(path) {
-    if(path === 'favorites') {
+    if (path === 'favorites') {
       console.log(path);
     }
   }
 
   render() {
     const main = document.createElement('div');
-    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
     this.app.innerHTML = '';
     this.app.append(main);
-    this.appState.favorites.push('3')
+    this.renderHeader();
+  }
+
+  renderHeader() {
+    const header = new Header(this.appState).render();
+    this.app.prepend(header);
   }
 }
