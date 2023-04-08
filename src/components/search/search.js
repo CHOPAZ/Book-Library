664fa540f8679883dbf0a10091fe3dbaeae6f77d
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-04-08 22:02:19 +0500

    create search component

---
+++ src/components/search/search.js
@@ -0,0 +1,29 @@
+import { DivComponent } from '../../common/div-component';
+import './search.css';
+
+export class Search extends DivComponent {
+  constructor(state) {
+    super();
+    this.state = state;
+  }
+
+  render() {
+    this.el.classList.add('search');
+    this.el.innerHTML = `
+      <div class="search__wrapper">
+        <input
+          type="text"
+          placeholder="Найти книгу или автора..."
+          class="search__input"
+          value="${this.state.searchQuery ? this.state.searchQuery : ''}"
+          />
+          <img src="/static/search.svg" alt="search" />
+      </div>
+      <button aria-lable="Искать">
+      <img src="./static/search-white.svg" alt="search-btn"/>
+      </button>
+
+    `;
+    return this.el;
+  }
+}
