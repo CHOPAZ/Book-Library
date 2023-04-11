7aa242965cc96a6807c1908b722705d71ffbbf6a
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-04-11 19:54:21 +0500

    add books in favorites

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -23,7 +23,7 @@ export class MainView extends AbstractView {
   /* Метод после обновления appState*/
   appStateHook(path) {
     if (path === 'favorites') {
-      console.log(path);
+      this.render()
     }
   }
 
